c200943023057469c7acc7e0758ee0632de5ae35
Maicon Ruas <maicon_ruas@hotmail.com>
2023-04-12 15:55:21 -0300

    Criando o .env

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express, { json } from 'express';
 import userRoute from './src/routes/user.route.js';
 import connectDatabase from './src/database/db.js';
+import dotenv from 'dotenv';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+dotenv.config();
 connectDatabase()
+
 app.use(json());
 app.use('/user', userRoute);
 
--- src/database/db.js
+++ src/database/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 const connectDatabase = () => {
     console.log("Wait connecting to the database")
 
-    mongoose.connect(
-        "mongodb+srv://root:root@cluster0.vnnvr4y.mongodb.net/?retryWrites=true&w=majority",
-        { useNewUrlParser: true, useUnifiedTopology: true }
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }
     ).then(() => console.log("MongoDB Atlas Connected")).catch((error) => console.log(error))
 };
 
