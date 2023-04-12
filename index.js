7119bd9294945c45ce03b7908697bdd884c85bcf
Maicon Ruas <maicon_ruas@hotmail.com>
2023-04-12 12:30:04 -0300

    Criação da conexão do banco

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const userRoute = require('./src/routes/user.route');
+const connectDatabase = require('./src/database/db');
+
 const app = express();
 const port = 3000;
 
+connectDatabase()
 app.use(express.json());
 app.use('/user', userRoute);
 
---
+++ src/database/db.js
@@ -0,0 +1,13 @@
+const { error } = require("console");
+const mongoose = require("mongoose");
+
+const connectDatabase = () => {
+    console.log("Wait connecting to the database")
+
+    mongoose.connect(
+        "mongodb+srv://root:root@cluster0.vnnvr4y.mongodb.net/?retryWrites=true&w=majority",
+        { useNewUrlParser: true, useUnifiedTopology: true }
+    ).then(() => console.log("MongoDB Atlas Connected")).catch((error) => console.log(error))
+};
+
+module.exports = connectDatabase;
