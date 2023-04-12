2904bb136ba410a9d4ad303e592cf29d67931423
Maicon Ruas <maicon_ruas@hotmail.com>
2023-04-12 15:42:42 -0300

    Adicionando bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -7,16 +8,19 @@ const UserSchema = new mongoose.Schema({
     },
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        select: false
     },
     avatar: {
         type: String,
@@ -27,6 +31,11 @@ const UserSchema = new mongoose.Schema({
         required: true
     }
 });
+
+UserSchema.pre("save", async function (next) {
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+  })
 
 const User = mongoose.model("User", UserSchema);
 
