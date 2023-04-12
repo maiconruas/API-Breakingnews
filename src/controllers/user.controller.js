7726c3711abc4c10adb6f4811ac638dccc4723cc
Maicon Ruas <maicon_ruas@hotmail.com>
2023-04-12 12:39:18 -0300

    criação do service User

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,6 @@
+const userService = require('../services/user.service');
 
-const create = (req, res) => {
+const create = async (req, res) => {
     const { name, username, email, password, avatar, background } = req.body;
 
     if (!name || !username || !email || !password || !avatar || !background) {
@@ -7,17 +8,22 @@ const create = (req, res) => {
     }
 
 
+    const user = await userService.create(req.body);
+
+    if (!user) {
+        return res.status(400).send({ message: "Error creating User" })
+    }
 
     res.status(201).send({
         message: "User created successfully",
         user: {
+            id: user._id,
             name,
             username,
             email,
             avatar,
             background
         },
-        timestamp: Date.now()
     });
 };
 
