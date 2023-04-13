589dcff745759e10f0da5ada41c77e6b0cecef47
Maicon Ruas <maicon_ruas@hotmail.com>
2023-04-13 16:28:19 -0300

    Ajuste

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import connectDatabase from './database/db.js';
 import userRoute from './routes/user.route.js';
 import authRoute from './routes/auth.routes.js';
 import newsRoute from './routes/news.route.js';
-import swaggerRoute from "./routes/swagger.route.cjs";
+import swaggerRoute from "./routes/swagger.route.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
