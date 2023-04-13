e56af77dcd45b98f2c9455c8f612a6e23bb7bf82
Maicon Ruas <maicon_ruas@hotmail.com>
2023-04-13 11:27:14 -0300

    Adicionando Paginação

--- src/controllers/news.controller.js
+++ src/controllers/news.controller.js
@@ -23,12 +23,51 @@ const create = async (req, res) => {
 };
 
 const findAll = async (req, res) => {
-    const news = await newsService.findAllService();
-    
+    let { limit, offset } = req.query;
+
+    limit = Number(limit);
+    offset = Number(offset);
+
+    if (!limit) {
+        limit = 5;
+    }
+
+    if (!offset) {
+        offset = 0;
+    }
+
+    const news = await newsService.findAllService(offset, limit);
+    const total = await newsService.countNews();
+    const currentUrl = req.baseUrl;
+
+    const next = offset + limit;
+    const nextUrl = next < total ? `${currentUrl}?limit=${limit}&offset=${next}` : null;
+
+    const previous = offset - limit < 0 ? null : offset - limit;
+    const previousUrl = previous != null ? `${currentUrl}?limit=${limit}&offset=${previous}` : null;
+
     if (news.length === 0) {
         return res.status(400).send({ message: "there are no registered news" })
     }
-    return res.send(news);
+    return res.send({
+        nextUrl,
+        previousUrl,
+        limit,
+        offset,
+        total,
+
+        results: news.map(item => ({
+            id: item._id,
+            title: item.title,
+            text: item.text,
+            banner: item.banner,
+            likes: item.likes,
+            comments: item.comments,
+            name: item.user.name,
+            username: item.user.username,
+            userAvatar: item.user.avatar,
+        }))
+    });
 };
 
 export default { create, findAll };
--- src/services/news.service.js
+++ src/services/news.service.js
@@ -2,7 +2,8 @@ import News from '../models/News.js';
 
 const createService = (body) => News.create(body);
 
-const findAllService = () => News.find();
+const findAllService = (offset, limit) => News.find().sort({ _id: -1 }).skip(offset).limit(limit).populate("user");
 
+const countNews = () => News.countDocuments();
 
-export default { createService, findAllService };
+export default { createService, findAllService, countNews };
