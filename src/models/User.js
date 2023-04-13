96829cc896a1d8097c4447b620fd194f2fee4445
Maicon Ruas <maicon_ruas@hotmail.com>
2023-04-13 09:07:12 -0300

    Criando novo model de News

--- src/models/User.js
+++ src/models/User.js
@@ -4,7 +4,7 @@ import bcrypt from 'bcrypt';
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
     },
     username: {
         type: String,
